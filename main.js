a5f73b5bcd38ccf84e967b8bf7279ca6a0e9dc14
vaanya2011 <100355303+vaanya2011@users.noreply.github.com>
2023-05-26 17:39:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload()
+{
+
+
+}
+
+
+function setup()
+{
+
+    canvas=createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+
+
+}
+
+function take_snapshot()
+{
+
+    save("mypic.png");
+}
